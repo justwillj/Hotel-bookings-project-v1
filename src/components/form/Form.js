056563f976926e3e5f9ad4ea5c5f19a154473bcd
Justin Williams <williams.jman@gmail.com>
2023-03-09 22:48:09 -0500

    Added more to our forms

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom/dist";
 import Input from "../input/Input";
 import "./Form.css";
 const Form = () => {
-  const formSubmit = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(false);
+  //Link that helped me with this
+  //https://bobbyhadz.com/blog/react-onclick-redirect
+  const navigate = useNavigate();
+
+  const formSubmit = () => {
+    navigate("/reservations");
+  };
   return (
     <div className="main">
       <form onSubmit={formSubmit} noValidate>
         <h1>Login</h1>
+        {error ? <p className="error">Invalid email or password</p> : null}
         <Input label="Email:" type="email" />
         <Input label="Password:" type="email" />
-        <button>Login</button>
+        <button onClick={formSubmit}>Login</button>
       </form>
     </div>
   );
