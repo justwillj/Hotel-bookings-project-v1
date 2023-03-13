a03bb94714ddfcd4a9e35ec1f26d964e7328d54e
Justin Williams <williams.jman@gmail.com>
2023-03-12 21:06:34 -0400

    Added more css to the forms

--- src/components/createReservation/CreateReservation.js
+++ src/components/createReservation/CreateReservation.js
@@ -1,15 +1,16 @@
+import Button from "../button/Button";
 import Input from "../input/Input";
-
+import "./CreateReservations.css";
 const CreateReservation = () => {
   return (
     <div>
       <h1>Create Reservation Page</h1>
       <form className="main">
         <Input label="Guest Email:" type="email" />
-        <Input label="Check-in date" type="text" />
-        <Input label="Number of nights" type="number" />
-        <Input label="Room Type" type="select" />
-        <button>Create</button>
+        <Input label="Check-in date:" type="text" />
+        <Input label="Number of nights:" type="number" />
+        <Input label="Room Type:" type="select" />
+        <Button value="Create" className="create-btn" />
       </form>
     </div>
   );
