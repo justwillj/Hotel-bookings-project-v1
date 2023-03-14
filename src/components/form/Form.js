802ad80ff778d8f304cfabb9c051c82b21b802d8
Justin Williams <williams.jman@gmail.com>
2023-03-13 21:39:05 -0400

    Created the basic logic for the login page

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom/dist";
 import Button from "../button/Button";
@@ -12,18 +13,62 @@ const Form = () => {
   //https://bobbyhadz.com/blog/react-onclick-redirect
   const navigate = useNavigate();
 
-  const formSubmit = () => {
-    navigate("/reservations");
+  const formLogin = (e) => {
+    e.preventDefault();
+
+    //Link that helped me with this
+    //https://blog.logrocket.com/how-to-use-axios-post-requests/
+    axios
+      .post("https://reqres.in/api/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        const role = res.data.role;
+        console.log(role);
+        console.log(res);
+        navigate("/reservations");
+      })
+      .catch((err) => {
+        setError(true);
+      });
+    // navigate("/reservations");
   };
+
+  const InputOnChange = (e) => {
+    //get the name from the input
+    const inputName = e.target.name;
+    //update state variables accordingly
+    switch (inputName) {
+      case "Email:":
+        setEmail(e.target.value);
+        break;
+      case "Password:":
+        setPassword(e.target.value);
+        break;
+      default:
+    }
+  };
+
   return (
     <div className="login-main">
       <div className="main">
-        <form onSubmit={formSubmit} noValidate>
+        <form onSubmit={formLogin} noValidate>
           <h1>Login</h1>
           {error ? <p className="error">Invalid email or password</p> : null}
-          <Input label="Email:" type="email" />
-          <Input label="Password:" type="email" />
-          <Button className="login-btn" onClick={formSubmit} value="Login" />
+          <Input
+            label="Email:"
+            type="email"
+            value={email}
+            onChange={InputOnChange}
+          />
+          <Input
+            label="Password:"
+            type="email"
+            value={password}
+            onChange={InputOnChange}
+          />
+          <Button className="login-btn" onClick={formLogin} value="Login" />
         </form>
       </div>
     </div>
